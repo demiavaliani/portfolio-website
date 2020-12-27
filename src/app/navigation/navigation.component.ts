3c79b9f141e83f2db2b0f059a89f6fef7288095a
Demi Avaliani <demikoavaliani@gmail.com>
2020-12-27 23:52:41 +0200

    Changes to Navigation component 1) Used Bootstrap Modal instead of Navbar 2) No JS needed for Modal color changes 3) CSS changed too

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -8,31 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
   constructor() {}
 
-  // changes navbar color on navbar icon click
-  changeNavbarColor = () => {
-    const navBar = document.querySelector('.navbar-custom');
-    const targetNode = document.getElementById('navBarBtn');
-
-    const config = {
-      attributeOldValue: true,
-      attributeFilter: ['class'],
-    };
-
-    const callback = (mutationList) => {
-      for (const mutation in mutationList) {
-        if (mutationList[mutation].oldValue.includes('collapsed')) {
-          navBar.setAttribute('style', 'background-color: red;');
-        } else if (!mutationList[mutation].oldValue.includes('collapsed')) {
-          navBar.setAttribute('style', 'background-color: transparent;');
-        }
-      }
-    };
-
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-  };
-
-  ngOnInit(): void {
-    this.changeNavbarColor();
-  }
+  ngOnInit(): void {}
 }
