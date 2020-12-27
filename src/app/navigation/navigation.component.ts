38ebfddf38501fb2c5ae7a7ca4fb0ff3f108522e
Demi Avaliani <demikoavaliani@gmail.com>
2020-12-27 23:47:54 +0200

    Navigation component 1) Navbar HTML 2) Navbar CSS 3) Navbar JS - full width, color changes on menu button click

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -3,24 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  // changes navbar color on navbar icon click
+  changeNavbarColor = () => {
+    const navBar = document.querySelector('.navbar-custom');
+    const targetNode = document.getElementById('navBarBtn');
 
-  currentCount = 0;
+    const config = {
+      attributeOldValue: true,
+      attributeFilter: ['class'],
+    };
 
-  counter() {
-    this.currentCount++;
-  }
+    const callback = (mutationList) => {
+      for (const mutation in mutationList) {
+        if (mutationList[mutation].oldValue.includes('collapsed')) {
+          navBar.setAttribute('style', 'background-color: red;');
+        } else if (!mutationList[mutation].oldValue.includes('collapsed')) {
+          navBar.setAttribute('style', 'background-color: transparent;');
+        }
+      }
+    };
 
-  counterMinus() {
-    this.currentCount--;
-  }
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  };
 
   ngOnInit(): void {
-    
+    this.changeNavbarColor();
   }
-
 }
