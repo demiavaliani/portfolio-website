9d49126fc9c65ae72dd053957c3d4ea5f5cb9841
Demi Avaliani <demikoavaliani@gmail.com>
2021-01-03 16:33:24 +0200

    1) modal is working now. toggling button as well. 2) modal button changes from hamburger to "X"

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-navigation',
@@ -8,5 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  modalEl;
+  modal;
+  modalIsHidden = true;
+
+  // toggles the modal on button click
+  closeModal = () => {
+    this.modal.toggle();
+  };
+
+  // sets modalIsHidden to true or false based on _isShown property.
+  // *ngIf from HTML later chooses the correct icon based on this variable
+  changeModalIcon = (ev) => {
+    if (this.modal._isShown) {
+      this.modalIsHidden = false;
+    } else {
+      this.modalIsHidden = true;
+    }
+  };
+
+  ngOnInit(): void {
+    this.modalEl = document.querySelector('.modal');
+    this.modal = new bootstrap.Modal(this.modalEl);
+
+    this.modalEl.addEventListener(
+      'shown.bs.modal',
+      this.changeModalIcon,
+      false
+    );
+    this.modalEl.addEventListener(
+      'hidden.bs.modal',
+      this.changeModalIcon,
+      false
+    );
+  }
 }
