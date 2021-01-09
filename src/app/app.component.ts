764604a51b20b3a0f9eb222ef7010230f9e2d2fd
Demi Avaliani <demikoavaliani@gmail.com>
2021-01-09 18:42:30 +0200

    added custom cursor with JS in app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
+
   title = 'portfolio-website';
+
+  ngOnInit(): void {
+    document.addEventListener('mousemove', (e) => {
+      const myCursor = document.getElementById('cursor');
+      myCursor.style.visibility = 'visible';
+      const x = e.pageX;
+      const y = e.pageY;
+
+      myCursor.style.transform = `translate(${x - 5}px, ${y - 5}px)`;
+    });
+  }
 }
